6d3fc32569b74678b51e8287240b0299023bbb82
VincentMugondora <vinmugondora@gmail.com>
2025-02-17 13:17:00 +0200

    finished calculator

--- python_calculator.py
+++ python_calculator.py
@@ -1,4 +1,15 @@
 # python calculator
 
-operator = input("Enter an operator (+ - * /: )")
-num1 = input()
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print(num1, operator, num2)
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 + num2)
+elif operator == "/":
+    print(num1 / num2)
+elif operator == "*":
+    print(num1 * num2)

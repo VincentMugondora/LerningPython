65f565e25ebdc6bd34be0c9c80f0e3a32b5650dd
VincentMugondora <vinmugondora@gmail.com>
2025-02-17 13:10:16 +0200

    building a simple calculator

--- conditonals.py
+++ conditonals.py
@@ -1,9 +1,21 @@
-# if = Do some code only IF some condition is True
-#   Else do something else
+# # if = Do some code only IF some condition is True
+# #   Else do something else
+#
+# age = int(input("Enter your age: "))
+#
+# if age >= 100:
+#     print("Go and Die")
+# elif age < 0:
+#     print("Please enter a valid age.")
+# elif age >= 18:
+#     print("You are old enough to vote!")
+# else:
+#     print("Sorry, you are too young to vote.")
+#
 
-age = input("Enter your age: ")
+# response = input("Do you want to play a game? (yes/no): ")
+# if response == "yes":
+#     print("Great, let's play!")
+# else:
+#     print("Okay, maybe another time.")
 
-if int(age) >= 18:
-    print("You are old enough to vote!")
-else:
-    print("Sorry, you are too young to vote.")

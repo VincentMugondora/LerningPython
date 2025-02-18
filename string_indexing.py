d30ea138b380234acb0170ee14a6aa7dc8f0efe9
VincentMugondora <vinmugondora@gmail.com>
2025-02-18 11:10:07 +0200

    updated

---
+++ string_indexing.py
@@ -0,0 +1,19 @@
+#                   [start : end : step]
+
+credit_number = "1234-5678-9012-3456-7890"
+
+# print(credit_number[10])
+# print(credit_number[:10]) # starting index is inclusive ending index is exclusive prints from 0
+# print(credit_number[5:9]) # prints within specified indexes 5 is inclusive and 9 is exclusive
+# print(credit_number[5:])  # prints from specified index to end of string
+# print(credit_number[-6])  # negative prints from the last number of the string last number i -1
+# print(credit_number[::2])   # counts every second character if it 3 counts every third character and so forth
+
+# get last four digits of a credit card number
+
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+#
+# # REVERSE THE STRING
+# credit_number = credit_number[::-1]
+# print(credit_number)

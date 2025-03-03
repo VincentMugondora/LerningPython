3968a2957d4dbf2dbf8b6efab664e292bbc5cc9f
VincentMugondora <vinmugondora@gmail.com>
2025-03-03 15:14:43 +0200

    started list comprehensions

--- error_handling.py
+++ error_handling.py
@@ -1,1 +1,20 @@
+class JustNotCoolError(Exception):
+    pass
 
+x=2
+try:
+    raise JustNotCoolError('This is just not cool')
+    # raise Exception("l am a custom exception")
+    # print(x / 1)
+    # if not type(x) is str:
+        # raise TypeError('Only strings are allowed')
+except NameError:
+    print('Name error means something is probably undefined')
+except ZeroDivisionError:
+    print("Please don't devide by zero")
+except Exception as e:
+    print(f'An error occured: {e}')
+else:
+    print('All good!')
+finally:
+    print('This code block will always run with or without an error')
